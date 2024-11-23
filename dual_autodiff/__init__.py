b182b28fef94ae9d280d1d7f36abf6b9c09e7649
Fayzan03 <mahmoodfayzan2@gmail.com>
2024-11-23 23:20:19 +0000

    Initial commit

---
+++ dual_autodiff/__init__.py
@@ -0,0 +1,7 @@
+# uncomment the following lines after installing the package
+
+## Import version information for the package
+#from .version import __version__  # Ensure version.py contains this variable
+
+## Print the package version for confirmation (optional)
+#print(f"Company package version: {__version__}")
---
+++ dual_autodiff/dual.py
@@ -0,0 +1,6 @@
+class Dual:
+    def __init__(self, real, dual):
+        self.real=real # Real part of dual number
+        self.dual=dual # Dual part of dual number
+
+    
